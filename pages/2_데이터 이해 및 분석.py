3311d2be0450d570a8454296fb184c25d30e9c0d
dasomeda <qkrqhwjd93@snu.ac.kr>
2024-12-19 15:26:13 +0900

    Update 2_데이터 이해 및 분석.py

--- pages/2_데이터 이해 및 분석.py
+++ pages/2_데이터 이해 및 분석.py
@@ -3,12 +3,12 @@ import pandas as pd
 import folium
 from streamlit_folium import st_folium
 import matplotlib.pyplot as plt
-from matplotlib import rcParams
+from matplotlib import rc
 import numpy as np
 
 # matplotlib 기본 폰트로 설정
-rcParams.update({'font.family': 'DejaVu Sans'})
-plt.rcParams['axes.unicode_minus'] = False  # 마이너스 기호 깨짐 방지
+rc.update({'font.family': 'DejaVu Sans'})
+plt.rc['axes.unicode_minus'] = False  # 마이너스 기호 깨짐 방지
 
 # Main App Tabs
 st.set_page_config(page_title="데이터 이해 및 분석", layout="wide")
